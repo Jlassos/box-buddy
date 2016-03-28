0f60b4a260cc0a18b85626353633c997752ce3ec
James <Jlassos@gmail.com>
2016-03-28 02:54:38 -0700

    Added gamestates and better game window functionality

---
+++ src/MainMenu.js
@@ -0,0 +1,25 @@
+BoxBuddy.MainMenu =  function (game) {
+    //this.music = null
+}
+BoxBuddy.MainMenu.prototype = {
+    create: function () {
+
+        var nameLabel = this.add.text(this.world.centerX, this.world.centerY, 'Box Buddy', {
+            font: '50px Arial', fill: '#ffffff'
+        })
+
+        var startLabel = this.add.text(this.world.centerX + 50, this.world.centerY + 70, 'Click to start', {
+            font: '25px Arial',
+            fill: '#ffffff'
+        })
+    },
+    update: function () {
+      if (game.input.activePointer.isDown) {
+          this.startGame()
+      }
+    },
+    // Call play state
+    startGame: function () {
+        this.state.start('Game')
+    }
+}
--- src/boot.js
+++ src/boot.js
@@ -1,3 +1,73 @@
-/**
- * Created by Zen on 3/20/2016.
- */
+
+
+BoxBuddy = {
+
+
+}
+
+BoxBuddy.Boot = function (game) {
+
+}
+
+BoxBuddy.Boot.prototype = {
+    init: function () {
+
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = true;
+
+        if (this.game.device.desktop)
+        {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.setMinMax(480, 260, 1024, 768);
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+        }
+        else
+        {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.setMinMax(480, 260, 1024, 768);
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+            this.scale.forceOrientation(true, false);
+            this.scale.setResizeCallback(this.gameResized, this);
+            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+        }
+
+    },
+
+    preload: function () {
+        // Load Assets for Preloader here
+
+        // Like a loading bar or background image
+    },
+
+    create: function () {
+        // Start Physics
+        game.physics.startSystem(Phaser.Physics.ARCADE)
+
+        // Call Load state
+        this.state.start('Preloader');
+
+    },
+    // Not currently Used
+    // gameResized: function (width, height) {
+    // },
+
+    enterIncorrectOrientation: function () {
+
+        BoxBuddy.orientated = false;
+
+        document.getElementById('orientation').style.display = 'block';
+
+    },
+
+    leaveIncorrectOrientation: function () {
+
+        BoxBuddy.orientated = true;
+
+        document.getElementById('orientation').style.display = 'none';
+
+    }
+
+}
